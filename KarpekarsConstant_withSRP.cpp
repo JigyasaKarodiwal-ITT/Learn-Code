b4c791916c5e10cb96fc486d249bf53966b9e34d
JigyasaKarodiwal-ITT <148524413+JigyasaKarodiwal-ITT@users.noreply.github.com>
2024-01-30 16:43:44 +0530

    Add files via upload

---
+++ KarpekarsConstant_withSRP.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <algorithm>
+
+
+int getStepsToReachKaprekar(int num) {
+    int digits[4];
+    for (int i = 3; i >= 0; --i) {
+        digits[i] = num % 10;
+        num /= 10;
+    }
+
+    std::sort(digits, digits + 4);
+    int ascending = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3];
+    int descending = digits[3] * 1000 + digits[2] * 100 + digits[1] * 10 + digits[0];
+    
+    
+    return descending - ascending;
+}
+
+
+int kaprekar(int num) {
+    const int kaprekar_const = 6174;
+    int count = 0;
+
+    while (num != kaprekar_const && num != 0) {
+        int nextStep = getStepsToReachKaprekar(num);
+
+        
+        std::cout << num << " -> " << nextStep << std::endl;
+
+    
+        num = nextStep;
+        if ( num == 0 ) {
+            break;
+        }
+
+        count++;
+    }
+
+    return count;
+}
+int getUserInput() {
+    int num;
+    std::cout << "Enter a 4-digit number: ";
+    std::cin >> num;
+
+    return num;
+}
+
+void displayResult(int steps) {
+    std::cout << "Number of steps to reach 6174: " << steps << std::endl;
+}
+
+int main() {
+    int num = getUserInput();
+
+    if (num < 1000 || num > 9999) {
+        std::cerr << "Please enter a valid 4-digit number." << std::endl;
+        return 1;
+    }
+
+    int steps = kaprekar(num);
+    displayResult(steps);
+
+    return 0;
+}
