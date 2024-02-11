87a0dc0e49338e625555006de15aa7239abb95f6
JigyasaKarodiwal-ITT <148524413+JigyasaKarodiwal-ITT@users.noreply.github.com>
2024-02-11 21:45:19 +0530

    Update ResultDisplay.java

--- ResultDisplay.java
+++ ResultDisplay.java
@@ -1,6 +1,6 @@
 public class ResultDisplay {
     public static void show(int steps) {
-        System.out.println("Number of steps to reach 6174: " + steps);
+        System.out.println("Number of steps to reach "+ Constants.karpekarConstant +": " + steps);
     }
 }
 
