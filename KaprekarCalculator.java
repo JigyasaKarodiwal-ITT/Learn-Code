d3d0a977583279c4f49f2f2281f3d22b753450ac
JigyasaKarodiwal-ITT <148524413+JigyasaKarodiwal-ITT@users.noreply.github.com>
2024-02-11 21:40:41 +0530

    Update KaprekarCalculator.java

--- KaprekarCalculator.java
+++ KaprekarCalculator.java
@@ -9,9 +9,9 @@ public class KaprekarCalculator {
         }
 
         Arrays.sort(digits);
-        int ascending = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3];
-        int descending = digits[3] * 1000 + digits[2] * 100 + digits[1] * 10 + digits[0];
+        int ascendingNumber = digits[0] * 1000 + digits[1] * 100 + digits[2] * 10 + digits[3];
+        int descendingNumber = digits[3] * 1000 + digits[2] * 100 + digits[1] * 10 + digits[0];
 
-        return descending - ascending;
+        return descendingNumber - ascendingNumber;
     }
 }
