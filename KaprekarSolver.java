effb6d9f25b6adbfb80a5d91db2c1e55718a544f
JigyasaKarodiwal-ITT <148524413+JigyasaKarodiwal-ITT@users.noreply.github.com>
2024-02-11 21:43:02 +0530

    Update KaprekarSolver.java

--- KaprekarSolver.java
+++ KaprekarSolver.java
@@ -1,9 +1,8 @@
 public class KaprekarSolver {
     public static int stepCount(int num) {
-        final int kaprekarConst = 6174;
         int count = 0;
 
-        while (num != kaprekarConst && num != 0) {
+        while (num != Constants.kaprekarConstant && num != 0) {
             int nextStep = KaprekarCalculator.calculateDifference(num);
             System.out.println(num + " -> " + nextStep);
             num = nextStep;
