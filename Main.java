c684bbe6c8f222525df01cb10f6119c47c731171
JigyasaKarodiwal-ITT <148524413+JigyasaKarodiwal-ITT@users.noreply.github.com>
2024-04-29 23:53:21 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         UserDataAccess dataAccess = new DatabaseHandler(); 
         UserNotification notification = new NotificationHandler(); 
         UserHandler userHandler = new UserHandler(dataAccess, notification);
         Signup signup = new Signup(userHandler);
         Login login = new Login(userHandler);
 
+        runApplication(scanner, signup, login);
+    }
+
+    private static void runApplication(Scanner scanner, Signup signup, Login login) {
         while (true) {
-            System.out.println("Welcome to Login/Signup Application");
-            System.out.println("1. Login");
-            System.out.println("2. Signup");
-            System.out.println("3. Exit");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
+            printMenu();
+            int choice = getUserChoice(scanner);
+            
             switch (choice) {
                 case 1:
                     login.userLogin();
@@ -28,11 +28,32 @@ public class Main {
                     signup.userSignup();
                     break;
                 case 3:
-                    System.out.println("Exiting application...");
-                    System.exit(0);
+                    exitApplication();
+                    break;
                 default:
-                    System.out.println("Invalid choice, please try again.");
+                    displayInvalidChoiceMessage();
             }
         }
+    }
+
+    private static void printMenu() {
+        System.out.println("Welcome to Login/Signup Application");
+        System.out.println("1. Login");
+        System.out.println("2. Signup");
+        System.out.println("3. Exit");
+    }
+
+    private static int getUserChoice(Scanner scanner) {
+        System.out.print("Enter your choice: ");
+        return scanner.nextInt();
+    }
+
+    private static void exitApplication() {
+        System.out.println("Exiting application");
+        System.exit(0);
+    }
+
+    private static void displayInvalidChoiceMessage() {
+        System.out.println("Invalid choice, please try again.");
     }
 }
