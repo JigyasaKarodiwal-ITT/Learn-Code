e65fede57484817e9739fdce5de3ae0d1a7bf74b
Jigyasa Karodiwal <jigyasa.karodiwal@intimetec.com>
2024-06-21 18:25:04 +0530

    CRUD operations

---
+++ Client/userHandler/admin.py
@@ -0,0 +1,27 @@
+from userHandler.user import User
+from menuItems.food_menu import FoodMenu
+
+class Admin(User):
+    def __init__(self, id, name, role, server_communicator):
+        super().__init__(id, name, role, server_communicator)
+        self.food_menu = FoodMenu(server_communicator, self.role)
+
+    def user_menu(self):
+        print(f"\nWelcome {self.name}({self.role})!")
+        while True:
+            print("\n1. View Menu\n2. Add Menu Item\n3. Delete Menu Item\n4. Update Menu Item\n5. Exit/Logout")
+            choice = int(input("Enter a choice : "))
+            if(choice == 1):
+                self.food_menu.view_menu()
+            elif(choice == 2):
+                self.food_menu.add_food_item()
+            elif(choice == 3):
+                self.food_menu.delete_menu_item()
+            elif(choice == 4):
+                self.food_menu.update_menu_item()
+            elif(choice == 5):
+                print("\nEXITING...")
+                self.server_communicator.close_connection()
+                break
+
+
---
+++ Client/userHandler/employee.py
@@ -0,0 +1,22 @@
+from userHandler.user import User
+from menuItems.food_menu import FoodMenu
+
+class Employee(User):
+    def __init__(self, id, name, role, server_communicator):
+        super().__init__(id, name, role, server_communicator)
+        self.food_menu = FoodMenu(server_communicator, self.role)
+
+    def user_menu(self):
+        print(f"\nWelcome {self.name}({self.role})!")
+        print("\n1. Food Selection\n2. See Feedback\n4. Exit/Logout")
+        choice = int(input("Enter a choice : "))
+        if(choice == 1):
+            print("Food Selection")
+        elif(choice == 2):
+            self.food_menu.view_feedback()
+        elif(choice == 4):
+            print("\nEXITING...")
+            self.client_socket.close_connection()
+            exit
+
+
